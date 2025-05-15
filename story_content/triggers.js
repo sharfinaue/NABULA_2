4f4bc05d2f59d0a2446a911bd635ac0c22fee9b7
ligjnj88 <sharfinaazmi@gmail.com>
2025-05-14 20:50:55 -0700

    File Upload

---
+++ story_content/triggers.js
@@ -0,0 +1,75 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5gD5fCxa1rJ":
+        Script1();
+        break;
+      case "6NTdd4qNuHF":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  const target = object('6IXCKzLMXJM');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '6UrC8wih2lM';
+const shakeAmount = 2;
+player.addForTriggers(
+id,
+target.animate([
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script2 = function()
+{
+  const target = object('6aWVvU68dIp');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '6MZ9kcQTOxU';
+const shakeAmount = 2;
+player.addForTriggers(
+id,
+target.animate([
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
